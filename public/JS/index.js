e734710550015959b67d573862b95dc87b552ec5
Samuel Rochwerg <samuelrochwerg1@gmail.com>
2021-04-13 18:17:31 +0200

    add JS

---
+++ public/JS/index.js
@@ -0,0 +1,17 @@
+fetch("http://localhost:3000/api/cameras")
+.then(response => response.json())
+.then(response => {
+    console.log(response)
+ 
+})
+.catch(error => alert("Erreur : " + error));
+
+console.log({hello:"hello"})
+
+const addition = (nombre1, nombre2) => {
+    let resultat = nombre1 + nombre2;
+    return resultat;
+}
+
+
+console.log(addition(10, 25))
