a1cc36ca7a824734399898a84bdcf148274a858e
Samuel Rochwerg <samuelrochwerg1@gmail.com>
2021-04-15 19:33:42 +0200

    tests JS

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,21 +1,44 @@
-const footer = document.querySelector("footer");
-footer.style.backgroundColor = "red";
+// const footer = document.querySelector("footer");
+// footer.style.backgroundColor = "red";
 
-fetch("http://localhost:3000/api/cameras")
-.then(response => response.json())
-.then(response => {
-    console.log(response)
+// fetch("http://localhost:3000/api/cameras")
+// .then(response => response.json())
+// .then(response => {
+//     console.log(response)
  
-})
-.catch(error => alert("Erreur : " + error));
+// })
+// .catch(error => alert("Erreur : " + error));
 
-console.log({hello:"hello"})
 
-const addition = (nombre1, nombre2) => {
-    let resultat = nombre1 + nombre2;
-    return resultat;
+// const produitCamera = document.querySelector(".section_produits_camera__name h3");
+// produitCamera.style.backgroundColor = "red";
+
+main()
+
+async function main() {
+    const articles = await getArticles()
+
+    for (article of articles) {
+        displayArticle(article)
+    }
 }
 
+function getArticles() {
+   return fetch("http://localhost:3000/api/cameras")
+   .then(function(httpBodyResponse) {
+       return httpBodyResponse.json()
+   })
+   .then(function(articles) {
+    return articles
+   })
+   .catch(function(error) {
+    alert(error)
+   })
+}
 
-console.log(addition(10, 25))
+function displayArticle() {
+    const templateElt = document.getElementById("templateArticle")
+    const cloneElt = document.importNode(templateElt.content, true)
 
+    cloneElt.getElementById
+}
