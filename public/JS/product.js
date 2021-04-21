64a6b7fae52207c3e8b7ad998ef41c3c8432edab
Samuel Rochwerg <samuelrochwerg1@gmail.com>
2021-04-21 17:15:36 +0200

    updates JS

--- public/JS/product.js
+++ public/JS/product.js
@@ -10,3 +10,12 @@ var url_string = window.location.href; //window.location.href
 var url = new URL(url_string);
 var id = url.searchParams.get("id");
 console.log(id);
+
+function getProductData(articleId) {
+    return fetch(`$http://localhost:3000/api/cameras/${articleId}`)
+      .catch((error) => {
+        console.log(error)
+      })
+      .then((httpBodyResponse) => httpBodyResponse.json())
+      .then((productData) => productData)
+  }
