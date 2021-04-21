1004b7829119524712831227fa2d530df8f9e9e4
Samuel Rochwerg <samuelrochwerg1@gmail.com>
2021-04-21 12:12:52 +0200

    ajout id url

--- public/JS/product.js
+++ public/JS/product.js
@@ -5,3 +5,8 @@ fetch("http://localhost:3000/api/cameras")
  
 })
 .catch(error => alert("Erreur : " + error));
+
+var url_string = window.location.href; //window.location.href
+var url = new URL(url_string);
+var id = url.searchParams.get("id");
+console.log(id);
